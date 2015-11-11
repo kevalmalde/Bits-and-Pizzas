88e689657a6e4c63152ac0a460032a98f00542cf
Keval <keval.malde@gmail.com>
2015-11-11 08:41:13 +0530

    Pasta List Fragment

--- app/src/main/java/com/keval/bitsandpizzas/PastaFragment.java
+++ app/src/main/java/com/keval/bitsandpizzas/PastaFragment.java
@@ -1,26 +1,27 @@
 package com.keval.bitsandpizzas;
 
-import android.os.Bundle;
+
 import android.app.ListFragment;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 
-
 /**
  * A simple {@link ListFragment} subclass.
  */
-public class PizzaFragment extends ListFragment {
+public class PastaFragment extends ListFragment {
+
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(
                 inflater.getContext(),
                 android.R.layout.simple_list_item_1,
-                getResources().getStringArray(R.array.pizzas)
-        );
+                getResources().getStringArray(R.array.pasta));
         setListAdapter(adapter);
-        return super.onCreateView(inflater, container, savedInstanceState);
+        return super.onCreateView(inflater,container, savedInstanceState);
     }
 }
